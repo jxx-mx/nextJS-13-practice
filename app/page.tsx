d5aa7b2480921165a6d80d0c82c837a9f669fa0f
jxx-mx <fierce.unarmed_0l@icloud.com>
2023-04-18 15:25:47 +0900

    feat: db 연결

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
-function Home() {
-  return <div>init</div>;
+import { connectDB } from "../util/database";
+
+async function Home() {
+  const data = await getData();
+  return (
+    <div>
+      {data.map((res) => (
+        <>
+          <div>{res.title}</div>
+          <div>{res.content}</div>
+        </>
+      ))}
+    </div>
+  );
 }
 
 export default Home;
+
+async function getData() {
+  let client = await connectDB;
+  let db = client.db("next-practice");
+  return await db.collection("post").find().toArray();
+}
